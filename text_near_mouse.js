fee2b3d9b38d3b3df47afc53a11ebd3b02892b08
Shahzod Rahmon <30922251+MrShahzodbek@users.noreply.github.com>
2024-04-03 23:58:26 +0500

    Update text_near_mouse.js

--- text_near_mouse.js
+++ text_near_mouse.js
@@ -1,75 +1,68 @@
- <script language="javascript"> 
-
 // ENTER TEXT BELOW. CAN *NOT* INCLUDE NORMAL HTML CODE. 
-var text=' SHAHAKADEMIA.NET '; 
+const text = ' SHAHAKADEMIA.NET '; 
 
-var delay=40; // SPEED OF TRAIL 
-var Xoff=0; // PIXEL COUNT FROM THE LEFT OF THE CURSOR (- VALUES GO TO LEFT) 
-var Yoff=-30; // PIXEL COUNT FROM THE TOP OF THE CURSOR (- VALUES GO UP) 
-var txtw=14; // AMOUNT OF PIXEL SPACE EACH CHARACTER OCCUPIES 
-var beghtml='<font color="blue"><b>'; // OPTIONAL HTML CODE THAT EFFECTS WHOLE TEXT STRING SUCH AS FONT COLOR, SIZE, ETC. 
-var endhtml='</b></font>'; // END HTML CODE. MOSTLY USED IF ABOVE SETTING IS USED. 
+const delay = 40; // SPEED OF TRAIL 
+const Xoff = 0; // PIXEL COUNT FROM THE LEFT OF THE CURSOR (- VALUES GO TO LEFT) 
+const Yoff = -30; // PIXEL COUNT FROM THE TOP OF THE CURSOR (- VALUES GO UP) 
+const txtw = 14; // AMOUNT OF PIXEL SPACE EACH CHARACTER OCCUPIES 
 
 //********** NO NEED TO EDIT BELOW HERE **********\\ 
 
-ns4 = (navigator.appName.indexOf("Netscape")>=0 && document.layers)? true : false; 
-ie4 = (document.all && !document.getElementById)? true : false; 
-ie5 = (document.all && document.getElementById)? true : false; 
-ns6 = (document.getElementById && navigator.appName.indexOf("Netscape")>=0 )? true: false; 
-var txtA=new Array(); 
-text=text.split(''); 
-var x1=0; 
-var y1=-1000; 
-var t=''; 
+const isLayersSupported = !!document.layers;
+const isAllSupported = !!document.all;
+const isGetElementByIdSupported = !!document.getElementById;
 
-for(i=1;i<=text.length;i++){ 
-t+=(ns4)? '<layer name="txt'+i+'" top="-100" left="0" width="'+txtw+'" height="1">' : '<div id="txt'+i+'" style="position:absolute; top:-100px; left:0px; height:1px; width:'+txtw+'; visibility:visible;">'; 
-t+=beghtml+text[i-1]+endhtml; 
-t+=(ns4)? '</layer>' : '</div>'; 
+const txtA = new Array(); 
+const splitText = text.split(''); 
+let x1 = 0; 
+let y1 = -1000; 
+let t = ''; 
+
+for(let i = 1; i <= splitText.length; i++){ 
+    t += isLayersSupported ? `<layer name="txt${i}" top="-100" left="0" width="${txtw}" height="1">` : `<div id="txt${i}" style="position:absolute; top:-100px; left:0px; height:1px; width:${txtw}; visibility:visible;">`; 
+    t += `${splitText[i-1]}`; 
+    t += isLayersSupported ? '</layer>' : '</div>'; 
 } 
-document.write(t); 
+document.body.innerHTML += t; 
 
-function moveid(id,x,y){ 
-if(ns4)id.moveTo(x,y); 
-else{ 
-id.style.left=x+'px'; 
-id.style.top=y+'px'; 
-}} 
+function moveid(id, x, y){ 
+    if(isLayersSupported) id.moveTo(x, y); 
+    else{ 
+        id.style.left = `${x}px`; 
+        id.style.top = `${y}px`; 
+    }
+} 
 
 function animate(evt){ 
-x1=Xoff+((ie4||ie5)?event.clientX+document.body.scrollLeft:evt.pageX); 
-y1=Yoff+((ie4||ie5)?event.clientY+document.body.scrollTop:evt.pageY); 
+    x1 = Xoff + (isAllSupported ? event.clientX + document.body.scrollLeft : evt.pageX); 
+    y1 = Yoff + (isAllSupported ? event.clientY + document.body.scrollTop : evt.pageY); 
 } 
 
 function getidleft(id){ 
-if(ns4)return id.left; 
-else return parseInt(id.style.left); 
+    return isLayersSupported ? id.left : parseInt(id.style.left); 
 } 
 
 function getidtop(id){ 
-if(ns4)return id.top; 
-else return parseInt(id.style.top); 
+    return isLayersSupported ? id.top : parseInt(id.style.top); 
 } 
 
 function getwindowwidth(){ 
-if(ie4||ie5)return document.body.clientWidth+document.body.scrollLeft; 
-else return window.innerWidth+pageXOffset; 
+    return isAllSupported ? document.body.clientWidth + document.body.scrollLeft : window.innerWidth + pageXOffset; 
 } 
 
 function movetxts(){ 
-for(i=text.length;i>1;i=i-1){ 
-if(getidleft(txtA[i-1])+txtw*2>=getwindowwidth()){ 
-moveid(txtA[i-1],0,-1000); 
-moveid(txtA[i],0,-1000); 
-}else moveid(txtA[i], getidleft(txtA[i-1])+txtw, getidtop(txtA[i-1])); 
-} 
-moveid(txtA[1],x1,y1); 
+    for(let i = splitText.length; i > 1; i--){ 
+        if(getidleft(txtA[i-1]) + txtw * 2 >= getwindowwidth()){ 
+            moveid(txtA[i-1], 0, -1000); 
+            moveid(txtA[i], 0, -1000); 
+        } else moveid(txtA[i], getidleft(txtA[i-1]) + txtw, getidtop(txtA[i-1])); 
+    } 
+    moveid(txtA[1], x1, y1); 
 } 
 
-window.onload=function(){ 
-for(i=1;i<=text.length;i++)txtA[i]=(ns4)?document.layers['txt'+i]:(ie4)?document.all['txt'+i]:document.getElementById('txt'+i); 
-if(ns4)document.captureEvents(Event.MOUSEMOVE); 
-document.onmousemove=animate; 
-setInterval('movetxts()',delay); 
-} 
-</script>
+window.onload = function(){ 
+    for(let i = 1; i <= splitText.length; i++) txtA[i] = isLayersSupported ? document.layers[`txt${i}`] : (isAllSupported ? document.all[`txt${i}`] : document.getElementById(`txt${i}`)); 
+    if(isLayersSupported) document.captureEvents(Event.MOUSEMOVE); 
+    document.onmousemove = animate; 
+    setInterval(movetxts, delay); 
+}
